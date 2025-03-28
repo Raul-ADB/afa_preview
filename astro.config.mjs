5c794a0c765c77bcf6878da3b2a6c431b2a00334
adrian <adriansilverslatter@gmail.com>
2025-03-27 19:12:36 -0600

    correcion en el astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raul-adb.github.io/',
-  base: 'afa_prev',
+  base: 'afa_preview',
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
